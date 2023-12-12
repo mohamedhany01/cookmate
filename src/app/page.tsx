6c5e91313af65e5ad09e90cba33500df7ab50bc4
mohamedhany01 <mohamedhyouns@gmail.com>
2023-12-12 18:40:23 +0200

    Initial

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <main className='bg-mate-400'>
+      <h1 className='text-5xl font-bold text-mate-orange'>Hello CookMate</h1>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    colors: {
+      mate: {
+        orange: '#E36824',
+        green: '#5B7C75',
+        gray: '#363636',
+        black: '#1C1C1C',
+        white: '#FFFCF7',
+        offWhite: '#FDEED9',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
